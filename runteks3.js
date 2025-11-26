b1dbeb0a32d17442d453cc3fc7dfce720c77f706
SCPD-PRODUCTION <scpdproduction4@gmail.com>
2025-11-26 19:06:00 +0700

    Create runteks3.js

---
+++ runteks3.js
@@ -0,0 +1,5 @@
+// Data ini nanti bisa diubah oleh admin panel
+let judulKajian = "AGENDA KAJIAN: Kajian Ba'da Maghrib - Ustadz Muhammad - Live YouTube Masjid";
+
+// Tampilkan text ke layar
+document.getElementById("runteks3-text").innerText = judulKajian;

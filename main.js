e68f64b71ba964b90c1787b44f5b21dc3cfb1ec4
SCPD-PRODUCTION <scpdproduction4@gmail.com>
2025-11-29 14:11:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
-const API = "http://localhost:3000";
+const API = "http://localhost:6000";
 
-// RUNTEKS 1
-fetch(API + "/runteks1")
-    .then(r => r.json())
-    .then(d => document.getElementById("r1-text").innerText = d.text);
-
-// JADWAL SHOLAT FADE
-setInterval(() => {
-    fetch(API + "/jadwal-sholat")
-        .then(r => r.json())
-        .then(d => {
-            const el = document.getElementById("jadwal-sholat");
-            el.style.opacity = 0;
-            setTimeout(() => {
-                el.innerText = d.jadwal;
-                el.style.opacity = 1;
-            }, 400);
-        });
-}, 3500);
-
-// RUNTEKS 3
-fetch(API + "/runteks3")
+// RUNTEKS 
+fetch(API + "/Runteks")
     .then(r => r.json())
     .then(d => document.getElementById("r3-text").innerText = d.text);
 
 // BANNER
-fetch(API + "/banner")
+fetch(API + "/Banner")
+    .then(r => r.json())
+    .then(d => {
+        document.getElementById("banner-image").src = d.url;
+
+// Profil
+fetch(API + "/Profil")
     .then(r => r.json())
     .then(d => {
         document.getElementById("banner-image").src = d.url;
